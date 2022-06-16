5adff141e30d04f30ff4e19703d73bdd720ba189
igudy <goodnessigunma1@gmail.com>
2022-06-16 10:33:32 -0700

    Added Testimonials

--- src/components/testimonial/Testimonials.jsx
+++ src/components/testimonial/Testimonials.jsx
@@ -5,25 +5,70 @@ import AVTR2 from '../../assets/avatar2.jpg'
 import AVTR3 from '../../assets/avatar3.jpg'
 import AVTR4 from '../../assets/avatar4.jpg'
 
+
+// import Swiper core and required modules
+import { Pagination, Scrollbar, A11y } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: 'Tina Snow',
+    review: 'Lorem ipsum dolor sit amet consectetur'
+  },
+  {
+    avatar: AVTR2,
+    name: 'Jon Snow',
+    review: 'Lorem ipsum dolor sit amet consectetu'
+  },
+  {
+    avatar: AVTR3,
+    name: 'Faraday Snow',
+    review: 'Lorem ipsum dolor sit amet consectetu'
+  },
+  {
+    avatar: AVTR4,
+    name: 'Peter Snow',
+    review: 'Lorem ipsum dolor sit amet consectetu'
+  },
+]
+
 const Testimonial = () => {
   return (
     <section id='testimonial'>
       <h5>Review from clients</h5>
       <h2>Testimonials</h2>
 
-      <div className="container testimonials__container">
-        <article className="testimonial">
-          <div className="client__avatar">
-            <img src={AVTR1} alt="Avatar One" />
-            <h5 className='client__review'>Ernest Gee</h5>
-            <small className='client__review'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-            Blanditiis id tempore consequuntur aliquam! 
-            Repellat omnis sed porro harum animi, iste 
-            tempore itaque vitae eius reiciendis fugit ipsum, 
-            quas officia laudantium!</small>
-          </div>
-        </article>
-      </div>
+        <Swiper className="container testimonials__container"
+        // install Swiper modules
+        modules={[Pagination]}
+        spaceBetween={30}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}
+      >
+
+        {
+          data.map(({avatar, name, review}, index) => {
+            return(
+              <SwiperSlide key={index} className="testimonial">
+                <div className="client__avatar">
+                  <img src={avatar} alt={name} />
+                </div>
+                <h5 className='client__review'>{name}</h5>
+                <small className='client__review'>{review}</small>
+            
+            </SwiperSlide>
+            )
+          })
+        }
+      </Swiper>
     </section>
   )
 }
