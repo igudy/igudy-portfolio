06079a966fda42e66e0f656edfa59d102cb45c77
igudy <goodnessigunma1@gmail.com>
2022-06-16 00:49:05 -0700

    Added my experience

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,75 @@
 import React from 'react'
+import { BsPatchCheckFill } from 'react-icons/bs'
 import './experience.css'
 
 const Experience = () => {
   return (
     <section id='experience'>
-      Experience
+      <h5>What Skills I Have</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>FrontEnd Development</h3>
+
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Javascript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+          </div>
+        </div>
+
+
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>PHP</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>Laravel</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className='experience__details'>
+              <BsPatchCheckFill className="experience__details-icon"/>
+              <div>
+                <h4>MySQL</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+        </div>
+        </div>
+      </div>
     </section>
   )
 }
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,144 @@
 import React from 'react'
+import { BiCheck } from 'react-icons/bi'
 import './services.css'
 
 const Services = () => {
   return (
     <section id='services'>
-      Services
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className='service'>
+          <div className="serivce__head">
+            <h3>Graphic Design</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of Graphic Design */}
+
+        {/* Front End Development */}
+        <article className='service'>
+          <div className="serivce__head">
+            <h3>Graphic Design</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* End of Front End Development */}
+
+        {/* Web Development */}
+        <article className='service'>
+          <div className="serivce__head">
+            <h3>Graphic Design</h3>
+          </div>
+
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>           
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>            
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quidem illo iure assumenda praesentium at nesciunt vitae nulla, laborum eveniet blanditiis incidunt dolorum nobis amet iusto excepturi error ex aperiam deserunt.</p>
+            </li>
+          </ul>
+        </article>
+
+        {/* End of web development */}
+      </div>
     </section>
   )
 }
